cf96c7f3b965239ac785ab93d316eeddf3f5e867
Andrei-Alin Sarbu <andr56f9@edu.zealand.dk>
2022-10-27 11:17:28 +0200

    Manager and Main class was added

---
+++ Managers/MusicRecordsManager.cs
@@ -0,0 +1,23 @@
+﻿using DanmarksRadio.Models;
+
+namespace DanmarksRadio.Managers
+{
+    public class MusicRecordsManager
+    {       
+        private static readonly List<MusicRecord> Data = new List<MusicRecord>
+        {   
+             new MusicRecord {Title = "LaFamilia", Artist = "Sisu",  Duration = 204 , PublicationYear = 2003 },
+             new MusicRecord {Title = "HaarpCord", Artist = "Puya", Duration = 202, PublicationYear = 2005},
+             new MusicRecord {Title = "StradaVarius", Artist = "Cedry2k", Duration = 183, PublicationYear = 2002 },
+             new MusicRecord {Title = "HaarpCord", Artist = "Dan", Duration = 178, PublicationYear = 2003 }
+            // https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers
+        };
+
+        public List<MusicRecord> GetAll()
+        {
+            return new List<MusicRecord>(Data);
+            // copy constructor
+            // Callers should no get a reference to the Data object, but rather get a copy
+        }
+    }
+}
---
+++ Models/MusicRecord.cs
@@ -0,0 +1,67 @@
+﻿using System.Drawing;
+
+namespace DanmarksRadio.Models
+{
+    public class MusicRecord
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public int Duration { get; set; }
+        public int PublicationYear { get; set; }
+
+
+
+        public void ValidateTitle()
+        {
+            if (Title == null)
+            {
+                throw new ArgumentNullException("You need to type a title");
+            }
+            else if (Title.Length > 50)
+            {
+                throw new ArgumentOutOfRangeException("Title name is too long");
+            }
+            else if (Title.Length < 3)
+            {
+                throw new ArgumentOutOfRangeException("Title name is too short");
+            }
+        }
+
+        public void ValidateArtist()
+        {
+            if (Artist == null)
+            {
+                throw new ArgumentNullException("You need to type an artist");
+            }
+            else if (Artist.Length > 50)
+            {
+                throw new ArgumentOutOfRangeException("Artist name is too long");
+            }
+            else if (Artist.Length < 3)
+            {
+                throw new ArgumentOutOfRangeException("Artist name is too short");
+            }
+        }
+        public void ValidateDuration()
+        {
+            if (Duration == null)
+            {
+                throw new ArgumentNullException("The duration of the record cannot be null");
+            }
+        }
+        public void ValidateProductionYear()
+        {
+            if (PublicationYear == null)
+            {
+                throw new ArgumentNullException("The publication year cannot be null");
+            }
+        }
+
+        public void Validate()
+        {   ValidateTitle();
+            ValidateArtist();
+            ValidateDuration();
+            ValidateProductionYear();
+        }
+    }
+}
